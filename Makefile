376d470c6db11d8a7d5cee151d7f3e29f82d4c8e
Andre Telfer <andretelfer@gmail.com>
2023-05-27 22:02:26 -0400

    Added data processing scripts and examples

--- Makefile
+++ Makefile
@@ -27,13 +27,14 @@ requirements: test_environment
 
 ## Make Dataset
 data: requirements
-	$(PYTHON_INTERPRETER) src/data/make_dataset.py data/raw data/processed
+	$(PYTHON_INTERPRETER) src/data/make_dataset.py
 
 ## Delete all compiled Python files
 clean:
 	find . -type f -name "*.py[co]" -delete
 	find . -type d -name "__pycache__" -delete
-
+	find data/processed -type f -name "*.csv" -delete
+	
 ## Lint using flake8
 lint:
 	flake8 src
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,107 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import pandas as pd
+import numpy as np
+import random
+
+from tqdm import tqdm
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+
+def graft_signals(signal1, signal2):
+    """Combines two signals at a random point to generate a new signal
+    """
+    split_point = random.randint(0, len(signal1)-1)
+    return np.concatenate([signal1[:split_point], signal2[split_point:]])
+
+
+def temporal_shift_signal(signal, max=10):
+    """Applies a random shift along the x-axis
+    """
+    s = pd.Series(signal)
+    shift = random.randint(-max, max)
+    s = s.shift(shift).ffill().bfill()
+    return s.values
+
+
+def amplitude_shift_signal(signal, max=0.1):
+    """Applies a random shift along the y-axis
+    """
+    signal = signal + random.random()*max*2-max
+    return np.clip(signal, 0, 1)
+
+
+def add_noise(signal, scale=0.05):
+    """Applies multiplicative noise generated using a normal distribution
+    """
+    noise = np.random.normal(loc=1.0, scale=scale, size=len(signal))
+    signal = signal * noise
+    return np.clip(signal, 0, 1)
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('class_sample_size', default=100000, type=click.INT)
+@click.argument('seed', default=42, type=click.INT)
+def main(class_sample_size, seed):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    # Seed for consistency in creating augmented datasets
+    np.random.seed(seed)
+
+    # Load the dataframe
+    project_dir = Path(__file__).resolve().parents[2]
+    logger.info('loading raw data from mitbih_train.csv')
+    df = pd.read_csv(project_dir / 'data/raw/mitbih_train.csv', header=None)
+
+    # Rename the target column
+    df = df.rename(columns={df.columns[-1]: 'target'})
+    df.target = df.target.astype(int)
+
+    # For each class, generate new signals using the graft augmentation
+    generated_dfs = []
+    for c, class_df in df.groupby('target'):
+        logger.info(
+            f'generating {class_sample_size} samples for class: {c}')
+        indices = np.arange(len(class_df))
+        values = class_df.values[:, :-1]
+        signals = []
+        for _ in tqdm(range(class_sample_size), leave=False):
+            # Randomly sample two signals
+            i1, i2 = np.random.choice(indices, size=2)
+            signal1 = values[i1]
+            signal2 = values[i2]
+
+            # Apply augmentations
+            signal = graft_signals(signal1, signal2)
+            signals.append(signal)
+
+        # Add the class column
+        signals = pd.DataFrame(signals)
+        signals['target'] = c
+        generated_dfs.append(signals)
+
+    # Merge and save the separate class_dataframes
+    logger.info('saving processed data to mitbih_train.csv')
+    output_df = pd.concat(generated_dfs)
+    output_df.to_csv(
+        project_dir / 'data/processed/mitbih_train.csv', index=False)
+
+    # Rename last column as 'target' in the test set
+    logger.info('reading raw data from mitbih_test.csv')
+    df = pd.read_csv(project_dir / 'data/raw/mitbih_test.csv', header=None)
+    df = df.rename(columns={df.columns[-1]: 'target'})
+    df.target = df.target.astype(int)
+    logger.info('saving processed data to mitbih_test.csv')
+    output_df.to_csv(
+        project_dir / 'data/processed/mitbih_test.csv', index=False)
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
